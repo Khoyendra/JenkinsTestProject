2c1c9ef69ee5fa3330d6db7c86b4f7c48cc19a1a
Khoyendra <khoyendra.pande@gmail.com>
2018-06-10 00:39:29 +0530

    Addec java code

---
+++ JavaTestClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java;
+
+import java.util.logging.Logger;
+
+/**
+ * @author N!Ru
+ * 
+ */
+public class JavaTestClass {
+	public static void main(String[] args) {
+		System.out.println("Jenkins Loop Started -----------!!!");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Hello Jenkins---!!! " + i + 1);
+		}
+		System.out.println("Jenkins Loop Stopped -----------!!!");
+	}
+}
